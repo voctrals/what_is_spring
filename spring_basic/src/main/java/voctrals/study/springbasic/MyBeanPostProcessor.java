948b08f10ed6e75fccf296139d09d4144313a19a
liulei <lei.liu@17zuoye.com>
2019-11-04 16:39:01 +0800

    Ordered用来描述那个bean优先被加载

--- spring_basic/src/main/java/voctrals/study/springbasic/MyBeanPostProcessor.java
+++ spring_basic/src/main/java/voctrals/study/springbasic/MyBeanPostProcessor.java
@@ -3,13 +3,14 @@ package voctrals.study.springbasic;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
 
 /**
  * @author lei.liu
  * @since 19-10-30
  */
 @Slf4j
-public class MyBeanPostProcessor implements BeanPostProcessor {
+public class MyBeanPostProcessor implements BeanPostProcessor, Ordered {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -23,4 +24,11 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
         return bean;
     }
 
+    /**
+     * Ordered接口用来约束该接口被加载的顺序，数字越小越早被加载
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
